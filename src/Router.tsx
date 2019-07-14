5dda73b81ad1cc774b2f49fac70c1432726f967b
Sam <icosmvs@gmail.com>
2019-07-14 20:14:46 +0200

    added redirect from root to /books route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import App from './App';
-import NotFound from './components/not-found/NotFound.component';
 
 const Router = (): any => (
   <BrowserRouter>
     <Switch>
+      <Route path="/books" component={App} />
+      <Redirect from="/" to="/books" />
       <Route exact path="/" component={App} />
-      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
