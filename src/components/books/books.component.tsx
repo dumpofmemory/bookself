c8e6160cd1f0a8b00512b97e9870359e419e0354
Sam <icosmvs@gmail.com>
2020-06-05 20:50:05 +0200

    user validation, do not save duplicate users to the db - merge fix

--- src/components/books/books.component.tsx
+++ src/components/books/books.component.tsx
@@ -3,11 +3,11 @@ import Header from '../../components/header/header.component';
 import SelectedBookPreview from '../selected-book-preview/selected-book-preview.component';
 import { useBooks } from './books.hooks';
 import SearchBar from '../search/searchbar.component';
-import Login from '../login/login.component';
+import SignInPage from '../signin-page/signin-page.component';
 import * as firebase from 'firebase';
 import { firebaseApp } from '../../rebase';
 import { useQuery } from '@apollo/react-hooks';
-import { GET_USERS } from '../../graphql/user.query';
+import { GET_USERS, GET_USER } from '../../graphql/user.query';
 import { useMutation } from '@apollo/react-hooks';
 import { CREATE_USER } from '../../graphql/user.mutation';
 
@@ -26,22 +26,42 @@ export function Users(): JSX.Element {
   );
 }
 
+export function User({ props }: any): JSX.Element {
+  return (
+    <div>
+      <h3>Yo {props}!</h3>
+    </div>
+  );
+}
+
 const Books = (): JSX.Element => {
   const booksHook = useBooks();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [uid, setUID] = useState<string | null>('');
 
   const [createUser] = useMutation(CREATE_USER);
+  const { data } = useQuery(GET_USER, {
+    variables: { uid },
+  });
 
   const authHandler = async (authData: any) => {
-    await createUser({
-      variables: {
-        uid: authData.user.uid,
-        name: authData.user.displayName,
-        email: authData.user.email,
-        photoURL: authData.user.photoURL,
-        isNewUser: authData.user.isNewUser,
-      },
-    });
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    // const googleAuthToken = authData.credential.accessToken;
+
+    // console.log(authData);
+    // console.log(data);
+
+    if (!data) {
+      await createUser({
+        variables: {
+          uid: authData.user.uid,
+          name: authData.user.displayName,
+          email: authData.user.email,
+          photoURL: authData.user.photoURL,
+          isNewUser: authData.user.isNewUser,
+        },
+      });
+    }
   };
 
   const authenticate = (provider: any) => {
@@ -55,26 +75,34 @@ const Books = (): JSX.Element => {
   useEffect(() => {
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
-        // User is signed in.
+        const uid = user.uid;
+        setUID(uid);
         setIsAuthenticated(true);
-        console.log(isAuthenticated);
-        console.log(user);
       } else {
-        // No user is signed in.
         setIsAuthenticated(false);
-        console.log(user);
-        console.log(isAuthenticated);
       }
     });
-  }, [isAuthenticated]);
+  }, [isAuthenticated, setUID]);
+
+  const signOut = async () => {
+    try {
+      await firebase.auth().signOut();
+      setIsAuthenticated(false);
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   return (
     <div className="App">
       {!isAuthenticated ? (
-        <Login authenticate={authenticate} />
+        <SignInPage authenticate={authenticate} />
       ) : (
         <div className="">
           <Header />
+          <div className="sign-out">
+            <button onClick={() => signOut()}>Sign Out</button>
+          </div>
           <div className="searchbar">
             <SearchBar selectedBook={booksHook.book} onSelectBook={booksHook.onSelectBook} />
           </div>
@@ -85,6 +113,7 @@ const Books = (): JSX.Element => {
               <div className="book-preview-section">
                 <SelectedBookPreview selectedBook={booksHook.book} />
                 <Users />
+                <User props={!uid ? 'NONE' : uid} />
               </div>
             </section>
           </main>
