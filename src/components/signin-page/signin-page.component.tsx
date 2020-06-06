12f5d088c3a6aad5f7f7aa9f2c5db923a0881ef5
dumpofmemory <dumpofmemory@users.noreply.github.com>
2020-06-06 19:48:23 +0200

    Feature/style signin page (#28)
    * style signin-page

--- src/components/signin-page/signin-page.component.tsx
+++ src/components/signin-page/signin-page.component.tsx
@@ -1,21 +1,68 @@
 import React from 'react';
+import './signin-page.component.scss';
 
 export interface SignInPageProps {
   authenticate: (provider: any) => void;
 }
 
 const SignInPage = ({ authenticate }: SignInPageProps): JSX.Element => (
-  <>
-    <button className="btn btn-link" onClick={() => authenticate('Facebook')}>
-      Login with Facebook
-    </button>
-    <button className="btn btn-link" onClick={() => authenticate('Twitter')}>
-      Login with Twitter
-    </button>
-    <button className="btn btn-link" onClick={() => authenticate('Google')}>
-      Login with Google
-    </button>
-  </>
+  // <div classNameName="signin-page">
+  //   <div classNameName="social-icons">
+  //     <button classNameName="btn btn-link" onClick={() => authenticate('Facebook')}>
+  //       Login with Facebook
+  //     </button>
+  //     <button classNameName="btn btn-link" onClick={() => authenticate('Twitter')}>
+  //       Login with Twitter
+  //     </button>
+  //     <button classNameName="btn btn-link" onClick={() => authenticate('Google')}>
+  //       Login with Google
+  //     </button>
+  //   </div>
+  // </div>
+  <section className="SignInPage">
+    <div className="content">
+      <header>
+        <h1>BookSelf</h1>
+        <h6>Sign in with</h6>
+      </header>
+      <section>
+        <div className="social-login">
+          <button onClick={() => authenticate('Google')}>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              alt="google"
+              width="20"
+            />
+            <span>Google</span>
+          </button>
+          <button onClick={() => authenticate('Twitter')}>
+            <img
+              src="https://cdn.freebiesupply.com/logos/large/2x/twitter-logo-svg-vector.svg"
+              alt="twitter"
+              width="20"
+            />
+            <span>Twitter</span>
+          </button>
+        </div>
+        <form action="" className="login-form">
+          <div className="input-group">
+            <input type="text" placeholder="Username or Email" id="username" />
+          </div>
+          <div className="input-group">
+            <input type="password" placeholder="Password" id="password" />
+          </div>
+          <div className="input-group">
+            <button>Login</button>
+          </div>
+        </form>
+      </section>
+      <footer>
+        <a href="#" title="Forgot Password">
+          Forgot Password
+        </a>
+      </footer>
+    </div>
+  </section>
 );
 
 export default SignInPage;
